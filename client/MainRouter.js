fdae81889a73788cd2e333ef58f0a72647c6710e
seanarch <zhuqingyun@outlook.com>
2023-05-17 17:25:54 -0600

    Added menu, deleteuser page and updated mainrouter with profile

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -4,15 +4,19 @@ import Home from "./core/Home";
 import Users from "./user/Users";
 import Signup from "./user/Signup";
 import Signin from "./auth/Signin";
+import Profile from "./user/Profile";
+import Menu from "./core/Menu";
 
 const MainRouter = () => {
   return (
     <div>
+      <Menu />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/users" component={Users} />
         <Route path="/signup" component={Signup} />
         <Route path="/signin" component={Signin} />
+        <Route path="/user/:userId" component={Profile} />
       </Switch>
     </div>
   );
